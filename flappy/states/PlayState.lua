b098b90b6e2efe1e8f563f08a094664ea1e90965
lagiacrus44 <60444427+lagiacrus44@users.noreply.github.com>
2021-09-02 18:23:18 -0400

    random spawn interval

--- flappy/states/PlayState.lua
+++ flappy/states/PlayState.lua
@@ -21,6 +21,7 @@ function PlayState:init()
     self.bird = Bird()
     self.pipePairs = {}
     self.timer = 0
+    self.spawnInterval = math.random(2,3) -- spawn interval randomized from 1 to 3
     self.score = 0
 
     -- initialize our last recorded Y value for a gap placement to base other gaps off of
@@ -32,7 +33,7 @@ function PlayState:update(dt)
     self.timer = self.timer + dt
 
     -- spawn a new pipe pair every second and a half
-    if self.timer > 2 then
+    if self.timer > self.spawnInterval then
         -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
         -- no higher than 10 pixels below the top edge of the screen,
         -- and no lower than a gap length (90 pixels) from the bottom
@@ -45,6 +46,9 @@ function PlayState:update(dt)
 
         -- reset timer
         self.timer = 0
+
+        -- randomize the spawn interval again, only after a pipe pair has been drawn for the previous interval
+        self.spawnInterval = math.random(2,3)
     end
 
     -- for every pair of pipes..
